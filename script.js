636b8570a0d44a6925941cb8b4929c0b68efc39e
James Gay <jamesgayht@gmail.com>
2023-04-06 21:36:36 +0800

    completed find path function

--- script.js
+++ script.js
@@ -25,12 +25,12 @@ function createMaze(mazeSize = 50) {
   const $maze = $("<div>").addClass("maze");
   $(".container").append($maze);
   for (let i = 0; i < mazeSize; i++) {
-    const $mazeRow = $("<div>").addClass("row");
-    $mazeRow.attr("id", `row${i}`);
+    const $mazeRow = $("<div>").addClass("col");
+    $mazeRow.attr("id", `col${i}`);
 
     for (let j = 0; j < mazeSize; j++) {
-      const $mazeGrid = $("<div>").addClass(`row${i} grid`);
-      $mazeGrid.attr("id", `row${i}-grid${j}`);
+      const $mazeGrid = $("<div>").addClass(`col${i} grid`);
+      $mazeGrid.attr("id", `col${i}-row${j}`);
       $mazeRow.append($mazeGrid);
       $maze.append($mazeRow);
     }
@@ -40,6 +40,7 @@ function createMaze(mazeSize = 50) {
   $("#End-button").on("click", addEndPoint);
   $("#Obstacles-button").on("click", addObstacles);
   $("#Clear-button").on("click", clearMaze);
+  $("#Visualize-button").on("click", visualizePath);
 }
 
 function addButtons(buttonNamesArray) {
@@ -136,4 +137,134 @@ function clearMaze(element) {
   }
 }
 
+function visualizePath() {
+
+  if($('.start').length === 0 || $('.end').length === 0) {
+    alert("Please input a start and end point to visualize the path.")
+  }
+  else {
+    // checkAdjacentNodes()
+    findPath()
+  }
+
+  
+}
+
+function checkAdjacentNodes() {
+  const $start = $('.start')
+  const $end = $('.end')
+
+  console.info("start >>> ", $start)
+  console.info("end >>> ", $end)
+
+  let currentNodeID = $('.start').attr('id')
+
+  currentNodeIDArr = currentNodeID.split("-");
+  console.info("currentNodeID[0] >>> ", currentNodeIDArr[0])
+  console.info("currentNodeID[1] >>> ", currentNodeIDArr[1])
+
+  // gets the current node's row and col number
+  let currentNodeCol = currentNodeIDArr[0].match(/\d/g).join("");
+  let currentNodeRow = currentNodeIDArr[1].match(/\d/g).join("");
+  console.info("currentNodeCol >>> ", currentNodeCol)
+  console.info("currentNodeRow >>> ", currentNodeRow)
+
+  // check top node
+  if(currentNodeRow !== 0) {
+    let topNodeCol = currentNodeCol
+    let topNodeRow = currentNodeRow - 1
+    const $topNode = $(`#col${topNodeCol}-row${topNodeRow}`)
+    console.log("$topNode >>> ", $topNode)
+    $topNode.addClass("checked")
+  }
+
+  // check left node 
+  if(currentNodeCol !== 0) {
+    let leftNodeCol = currentNodeCol - 1
+    let leftNodeRow = currentNodeRow
+    const $leftNode = $(`#col${leftNodeCol}-row${leftNodeRow}`)
+    console.log("$leftNode >>> ", $leftNode)
+    $leftNode.addClass("checked")
+  }
+
+  // check right node
+  if(currentNodeCol !== $('.col').length - 1) {
+    let rightNodeCol = parseInt(currentNodeCol) + 1
+    let rightNodeRow = currentNodeRow
+
+    const $rightNode = $(`#col${rightNodeCol}-row${rightNodeRow}`)
+    console.log("$rightNode >>> ", $rightNode)
+    $rightNode.addClass("checked")
+  }
+
+  // check bottom node
+  if(currentNodeCol !== $('.col').length - 1) {
+    let bottomNodeCol = currentNodeCol
+    let bottomNodeRow = parseInt(currentNodeRow) + 1
+
+    const $bottomNode = $(`#col${bottomNodeCol}-row${bottomNodeRow}`)
+    console.log("$bottomNode >>> ", $bottomNode)
+    $bottomNode.addClass("checked")
+  }
+
+
+}
+
+function findPath() {
+  const startNodeID = $('.start').attr('id')
+  const $endNode = $('.end')
+  const endNodeID = $('.end').attr('id')
+
+  let startNodeIDArr = startNodeID.split("-")
+  let endNodeIDArr = endNodeID.split("-")
+
+  let startNodeCol = startNodeIDArr[0].match(/\d/g).join("");
+  let startNodeRow = startNodeIDArr[1].match(/\d/g).join("");
+  let endNodeCol = endNodeIDArr[0].match(/\d/g).join("");
+  let endNodeRow = endNodeIDArr[1].match(/\d/g).join("");
+
+  while($('.found').length < 1) {
+
+    if(startNodeCol < endNodeCol) {
+      const $rightNode = $(`#col${parseInt(startNodeCol)+1}-row${startNodeRow}`)
+      $rightNode.addClass('path')
+      startNodeCol++
+      if($rightNode.attr("id") === endNodeID) {
+        console.info("FOUND RIGHT")
+        $endNode.addClass('found')
+      }
+    }
+    else if(startNodeCol > endNodeCol) {
+      const $leftNode = $(`#col${parseInt(startNodeCol)-1}-row${startNodeRow}`)
+      $leftNode.addClass('path')
+      startNodeCol--
+      if($leftNode.attr("id") === endNodeID) {
+        console.info("FOUND LEFT")
+        $endNode.addClass('found')
+      }
+    }
+
+    else if(startNodeRow > endNodeRow) {
+      const $topNode = $(`#col${startNodeCol}-row${parseInt(startNodeRow) - 1}`)
+      $topNode.addClass('path')
+      startNodeRow--
+      if($topNode.attr("id") === endNodeID) {
+        console.info("FOUND TOP")
+        $endNode.addClass('found')
+      }
+    }
+
+    else if(startNodeRow < endNodeRow) {
+      const $bottomNode = $(`#col${startNodeCol}-row${parseInt(startNodeRow) + 1}`)
+      $bottomNode.addClass('path')
+      startNodeRow++
+      if($bottomNode.attr("id") === endNodeID) {
+        console.info("FOUND BOTTOM")
+        $endNode.addClass('found')
+      }
+    }
+
+  }
+
+}
 
